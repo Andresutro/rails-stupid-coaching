0caa96c3ab41c53d2e9e0b824e23a2c4b6b21c80
andres felipe marchant <andres.marchant.s@uc.cl>
2023-05-16 20:08:41 -0400

    ejercicio crudo

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,22 @@
+class QuestionsController < ApplicationController
+  def ask
+
+  end
+
+  def answer
+    @ask = params[:answer]
+
+    if @ask
+      if @ask == "I am going to work"
+        @answer = "Great!"
+      elsif @ask[-1] == "?"
+        @answer = "Silly question, get dressed and go to work!"
+      else
+        @answer = "I don't care, get dressed and go to work!"
+      end
+    end
+
+    puts @answer
+    
+  end
+end
